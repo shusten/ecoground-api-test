d476b649063e6b0acfa512cb7b44878c5d71bd0a
Luis Villas-Boas <shusten@hotmail.com>
2024-09-23 13:00:10 -0300

    first commit

---
+++ src/main/java/ecoground/camera/test/ping/PingController.java
@@ -0,0 +1,14 @@
+package ecoground.camera.test.ping;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String pong() {
+        return "pong";
+    }
+}
